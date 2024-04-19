6118c4599fdee75d944e5db3c7dd308d9d5be1b0
Abbas Ali <wwwabbasmughal786@gmail.com>
2024-04-19 21:03:16 +0500

    Update user_detail.dart

--- lib/user_detail.dart
+++ lib/user_detail.dart
@@ -8,7 +8,6 @@ class UserDetailsPage extends StatefulWidget {
   _UserDetailsPageState createState() => _UserDetailsPageState();
 }
 
-
 class _UserDetailsPageState extends State<UserDetailsPage> {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
