06e48bdc239693be098de75578f0ab1fbb26600b
Abbas Ali <wwwabbasmughal786@gmail.com>
2024-08-12 22:34:08 +0500

    Update AddToCart.dart

--- lib/AddToCart.dart
+++ lib/AddToCart.dart
@@ -4,7 +4,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:happy_meal/Cart_page.dart';
 import 'package:happy_meal/controller/provider.dart';
-import 'package:happy_meal/user_detail.dart';
 import 'package:provider/provider.dart';
 
 class AddToCart extends StatefulWidget {
@@ -85,82 +84,80 @@ class _AddToCartState extends State<AddToCart> {
         ],
       ),
 
-      body: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 20,),
+        
+          Image.asset('assets/images/Burger.png', width: 250,),
+          SizedBox(height: 10,),
+      
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(height: 20,),
-              Image.asset('assets/images/Burger.png', width: 250,),
-              SizedBox(height: 10,),
-
+              TextButton(
+                  onPressed: () {
+                    minus();
+                  },
+      
+                  child: Icon(CupertinoIcons.minus_circle,color: Colors.red, size: 40,)),
+              Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text('$items', style: TextStyle(fontSize: 30,)),
+              ),
+              TextButton(
+                  onPressed: () {
+                    add();
+                    result();
+                    // Add your item to the cart
+                    final cartProvider = Provider.of<CartProvider>(context, listen: false);
+                    cartProvider.addToCart(CartItem(itemName: 'Chicken Burger x1', price: 950.0));
+      
+                    // Show a snackbar or navigate to another screen
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text('Item added to cart', ),
+                        duration: Duration(seconds: 1),
+                      ),
+                    );
+                  },
+                  child: Icon(Icons.add_circle_outline, color: Colors.green, size: 40,)
+              ),
+            ],
+          ),
+      
+          Column(
+            children: [
+              SizedBox(height: 30,),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text('Click (+) for Tasty Meal', style: TextStyle(fontSize: 30, color: Colors.white)),
+                ),
+              ),
+          ],
+          ),
+      
+          Column(
+            children: [
+              SizedBox(height: 50,),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextButton(
-                      onPressed: () {
-                        minus();
-                      },
-
-                      child: Icon(CupertinoIcons.minus_circle,color: Colors.red, size: 40,)),
-                  Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: Text('$items', style: TextStyle(fontSize: 30,)),
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        add();
-                        result();
-                        // Add your item to the cart
-                        final cartProvider = Provider.of<CartProvider>(context, listen: false);
-                        cartProvider.addToCart(CartItem(itemName: 'Chicken Burger x1', price: 950.0));
-
-                        // Show a snackbar or navigate to another screen
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: Text('Item added to cart', ),
-                            duration: Duration(seconds: 1),
-                          ),
-                        );
-                      },
-                      child: Icon(Icons.add_circle_outline, color: Colors.green, size: 40,)
-                  ),
-                ],
+                  Text('Your Total Price is' , style: TextStyle(fontSize: 30,)),
+                  Text(' $total/-rs', style: TextStyle(fontSize: 30, color: Colors.red,)),
+      
+                ]
               ),
-
-              Column(
-                children: [
-                  SizedBox(height: 30,),
-                  Container(
-
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text('Click (+) for Tasty Meal', style: TextStyle(fontSize: 30, color: Colors.white)),
-                    ),
-                  ),
-              ],
-              ),
-
-              Column(
-                children: [
-                  SizedBox(height: 50,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Your Total Price is' , style: TextStyle(fontSize: 30,)),
-                      Text(' $total/-rs', style: TextStyle(fontSize: 30, color: Colors.red,)),
-
-                    ]
-                  ),
-                ],
-              ),
-
             ],
           ),
-        ),
+      
+        ],
+      ),
     );
   }
 }
