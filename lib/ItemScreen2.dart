03bf6c66cdf8f2c6470f8e65646306f140d64c09
Abbas Ali <wwwabbasmughal786@gmail.com>
2024-09-19 19:52:13 +0500

    Update ItemScreen2.dart

--- lib/ItemScreen2.dart
+++ lib/ItemScreen2.dart
@@ -32,6 +32,7 @@ class ItemScreen2 extends StatelessWidget {
                 return Stack(
                   alignment: Alignment.topRight,
                   children: [
+
                     
 
                     IconButton(
