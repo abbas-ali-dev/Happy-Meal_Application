156158bacc27ea0748a509605e77ddf4a04f842c
Abbas Ali <wwwabbasmughal786@gmail.com>
2025-03-12 22:34:42 +0500

    Update ItemScreen1.dart

--- lib/ItemScreen1.dart
+++ lib/ItemScreen1.dart
@@ -19,10 +19,15 @@ class ItemScreen1 extends StatelessWidget {
           onTap: () {
             Navigator.pop(context);
           },
-          child: Icon(Icons.arrow_back_ios,),
+          child: Icon(
+            Icons.arrow_back_ios,
+          ),
         ),
-        title: Text('Chicken Burger', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold,)),
-
+        title: Text('Chicken Burger',
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            )),
         actions: [
           Consumer<CartProvider>(
             builder: (context, cartProvider, child) {
@@ -43,16 +48,16 @@ class ItemScreen1 extends StatelessWidget {
                   ),
                   itemCount > 0
                       ? Positioned(
-                    right: 8,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.red,
-                      radius: 10,
-                      child: Text(
-                        itemCount.toString(),
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                  )
+                          right: 8,
+                          child: CircleAvatar(
+                            backgroundColor: Colors.red,
+                            radius: 10,
+                            child: Text(
+                              itemCount.toString(),
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          ),
+                        )
                       : SizedBox.shrink(),
                 ],
               );
@@ -60,35 +65,63 @@ class ItemScreen1 extends StatelessWidget {
           ),
         ],
       ),
-
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 10,),
-            Text('Mized Burger With Beef, Chilli & Spicy', style: TextStyle(fontSize: 17,),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Mized Burger With Beef, Chilli & Spicy',
+              style: TextStyle(
+                fontSize: 17,
+              ),
               textAlign: TextAlign.center,
             ),
-
-            SizedBox(height: 8, ),
+            SizedBox(
+              height: 8,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.star, color: Colors.red, size: 22,),
-                SizedBox(width: 2,),
-                Text('4.7', style: TextStyle(fontSize: 17),)
+                Icon(
+                  Icons.star,
+                  color: Colors.red,
+                  size: 22,
+                ),
+                SizedBox(
+                  width: 2,
+                ),
+                Text(
+                  '4.7',
+                  style: TextStyle(fontSize: 17),
+                )
               ],
             ),
-            Padding(padding: EdgeInsets.all(25),
-              child: Image.asset('assets/images/Burger.png',),
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: Image.asset(
+                'assets/images/Burger.png',
+              ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Column(
                   children: [
-                    Text("Calories", style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),),
-                    SizedBox(height: 5,),
-                    Text('130', style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),)
+                    Text(
+                      "Calories",
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      '130',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    )
                   ],
                 ),
                 Container(
@@ -97,59 +130,101 @@ class ItemScreen1 extends StatelessWidget {
                 ),
                 Column(
                   children: [
-                    Text('Size', style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),),
-                    SizedBox(height: 5,),
-                    Text('Double Pati', style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),)
+                    Text(
+                      'Size',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      'Double Pati',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                    )
                   ],
                 )
               ],
             ),
-
-            SizedBox(height: 40,),
-            Padding(padding: EdgeInsets.symmetric(horizontal: 20),
+            SizedBox(
+              height: 40,
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       children: [
-                        Text('Order', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-                        SizedBox(height: 3,),
+                        Text('Order',
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold)),
+                        SizedBox(
+                          height: 3,
+                        ),
                         Row(
                           children: [
-                            Text('Place', style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold)),
+                            Text('Place',
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.bold)),
                           ],
                         )
                       ],
                     ),
-
                     Column(
                       children: [
-                        Text('Delivery', style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),),
-                        SizedBox(height: 8,),
-                        Text('Express', style: TextStyle(fontSize: 17, color: Colors.green, fontWeight: FontWeight.bold),),
+                        Text(
+                          'Delivery',
+                          style: TextStyle(
+                              fontSize: 17, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          'Express',
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ],
                     ),
-
                     Column(
                       children: [
-                        Text('"Price"', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.green,)),
-                        Text('950/-rs', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.red,), ),
+                        Text('"Price"',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.green,
+                            )),
+                        Text(
+                          '950/-rs',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
+                        ),
                       ],
                     )
                   ],
                 )),
-
           ],
         ),
       ),
-
       bottomNavigationBar: InkWell(
         onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => AddToCart(),));
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AddToCart(),
+              ));
         },
         child: Container(
           height: 60,
-          margin: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
           padding: EdgeInsets.symmetric(vertical: 20),
           decoration: BoxDecoration(
             color: Colors.red,
@@ -158,24 +233,20 @@ class ItemScreen1 extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Add to Cart', style: TextStyle( fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold,)),
-              SizedBox(width: 8,),
+              Text('Add to Cart',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(
+                width: 8,
+              ),
               Icon(Icons.add_circle_outline_outlined, color: Colors.white),
             ],
-
           ),
         ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
